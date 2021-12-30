299295735b89c7d63b5c06926ea0a5c7ef2eb5bf
Shivanshu Mishra <smishra_be20@thapar.edu>
2021-12-30 11:11:34 +0530

    completed project

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(XylophoneApp());
@@ -8,8 +9,48 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: HomePage(),
         ),
+      ),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  final player=AudioCache();
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        MusicBtn(sound: 1, player: player, color: Colors.red),
+        MusicBtn(sound: 2, player: player, color: Colors.orange),
+        MusicBtn(sound: 3, player: player, color: Colors.yellow),
+        MusicBtn(sound: 4, player: player, color: Colors.blue),
+        MusicBtn(sound: 5, player: player, color: Colors.green),
+        MusicBtn(sound: 6, player: player, color: Colors.indigo),
+        MusicBtn(sound: 7, player: player, color: Colors.purple),
+      ],
+    );
+  }
+}
+
+class MusicBtn extends StatelessWidget {
+  final int sound;
+  final MaterialColor color;
+  final AudioCache player;
+  const MusicBtn({ @required this.sound, @required this.player, @required this.color});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: GestureDetector(
+        onTap: () => {
+          player.play("note$sound.wav")
+        },
+        child: Container(
+          color: color,
+        )
       ),
     );
   }
